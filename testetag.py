008a32bbef29171202329d3643e273c8492652ad
Nathália <np.nathaliapereira@gmail.com>
2022-04-24 15:16:11 -0300

    feat: Organiza o tabuleiro do sudoku em uma classe

--- testetag.py
+++ testetag.py
@@ -8,78 +8,75 @@ grid = [[5,3,0,0,7,0,0,0,0],
         [0,0,0,0,1,9,0,0,5],
         [0,0,0,0,0,0,0,0,0]]
 
-n, m = 9, 9
+class Sudoku():
+	def __init__(self):
+		self.adj = []
+		self.valid = True
+		self.all_vertices = []
 
-compact = []
-for i in range(n):
-	for j in range(m):
-		compact.append(grid[i][j])
+	def add_vertices(self, grid):
+		for i in range(9):
+			for j in range(9):
+				self.all_vertices.append(grid[i][j])
 
-# print(compact)
+		return self.all_vertices
 
-def to_adjacency(grid):
-	adj = []
-	for i in range(81):
-		adj.append([])
+	def is_valid(self, adj):
+		# confere se os valor dos vértices são menores ou iguais a 9
+		for vertex in self.all_vertices:
+			if (vertex < 0) or (vertex > 9):
+				self.valid = False
 
-	counter = 0;
+		for i in range(81):
+			for k in range(1,9):
+				if (adj[i].count(self.all_vertices[i]) > 1 and self.all_vertices[i] != 0):
+					self.valid = False
 
-	for i in range(n):
-		for j in range(m):
-
-			# row
-			for k in range(9):
-				if (k != j):
-					adj[counter].append(grid[i][k])
-
-			# columns
-			for k in range(9):
-				if (k != i):
-					adj[counter].append(grid[k][j])
-
-			# square
-			# print(counter)
-			# primeiro multiplo de 3 menor do que o número
-			m3 = counter - (counter % 3)
-
-			# linha e coluna do quadrado
-			square_i = (m3 // 27) * 3
-			square_j = m3 % 9
-
-			# print(square_i, square_j)
-
-			for a in range(square_i, square_i + 3):
-				for b in range(square_j, square_j + 3):
-					if (i != a) and (j != b):
-						adj[counter].append(grid[a][b])
+		return self.valid
 
 
+	def build_adjacency(self, grid):
+		for i in range(81):
+			self.adj.append([])
 
-			counter += 1
+		counter = 0;
 
-	return adj
+		for i in range(9):
+			for j in range(9):
 
-#adj = [
-#		[1, 2, 3, 4, 5, 6, 7, 8, 9, 18, 27, 36, 45, 54, 63, 72, 10, 11, 19, 20]
-#	  ]
+				# row
+				for k in range(9):
+					if (k != j):
+						self.adj[counter].append(grid[i][k])
 
-def valid_graph(grid, adj):
+				# columns
+				for k in range(9):
+					if (k != i):
+						self.adj[counter].append(grid[k][j])
 
-	# conferir se os numeros sao menores que 9
-	ans = True
-	for i in range(81):
-		for k in range(1,9):
-			if (adj[i].count(compact[i]) > 1 and compact[i] != 0):
-				# print(compact[i], adj[i])
-				# print(k, i, adj[i])
-				ans = False
+				# square
+				# print(counter)
+				# primeiro multiplo de 3 menor do que o número
+				m3 = counter - (counter % 3)
 
-	return ans
+				# linha e coluna do quadrado
+				square_i = (m3 // 27) * 3
+				square_j = m3 % 9
 
+				# print(square_i, square_j)
 
+				for a in range(square_i, square_i + 3):
+					for b in range(square_j, square_j + 3):
+						if (i != a) and (j != b):
+							self.adj[counter].append(grid[a][b])
 
+				counter += 1
 
-#print(valid_matrix(grid))
-adj = to_adjacency(grid)
-# print(adj)
-print(valid_graph(grid, adj))
+		return self.adj
+
+sudoku = Sudoku()
+sudoku.add_vertices(grid)
+adj = sudoku.build_adjacency(grid)
+valid = sudoku.is_valid(adj)
+
+print(valid)
